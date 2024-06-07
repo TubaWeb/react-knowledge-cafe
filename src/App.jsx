df9bfb287665a0de9af16116f28c6bc3689dbd25
TubaWeb <ziaulhoquek5@gmail.com>
2024-06-07 08:36:30 +0600

    start blogs componented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Blogs from './Component/Blogs/Blogs'
+import Bookmarks from './Component/Bookmarks/Bookmarks'
 import Header from './Component/Header/Header'
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
